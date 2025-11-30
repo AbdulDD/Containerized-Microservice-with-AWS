e83207459f17990090e61dbfa15c72cd85728b3a
Abdul Muqtadir <73823824+AbdulDD@users.noreply.github.com>
2025-11-30 18:06:34 +0000

    corrected the test files

--- test/test_ner_detector.py
+++ test/test_ner_detector.py
@@ -1,14 +1,16 @@
 import pytest
 from utils.ner_detector import ner_pipe
 
-# test samples
-text1 = "Microsoft"
-text2 = "John"
 
-# Inferences
-ner1 = ner_pipe(text1)
-ner2 = ner_pipe(text2)
+def test_ner_detector():
+    # test samples
+    text1 = "Microsoft"
+    text2 = "John"
 
-# assert the outptus
-assert ner1[0]['entity'] == 'B-ORG'
-assert ner2[0]['entity'] == 'B-'
+    # Inferences
+    ner1 = ner_pipe(text1)
+    ner2 = ner_pipe(text2)
+
+    # assert the outptus
+    assert ner1[0]['entity'] == 'B-ORG'
+    assert ner2[0]['entity'] == 'B-'
--- test/test_sentimental_analyzer.py
+++ test/test_sentimental_analyzer.py
@@ -1,18 +1,15 @@
-# Imports
 import pytest
 from utils.sentimental_analyzer import sentiment_analysis
 
-text1 = "I love MLOps"
-text2 = "I love AI"
-text3 = "I hate studing quantum computing"
+def test_sentiment_analysis():
+    text1 = "I love MLOps"
+    text2 = "I love AI"
+    text3 = "I hate studying quantum computing"
 
+    sentiment1 = sentiment_analysis(text1)
+    sentiment2 = sentiment_analysis(text2)
+    sentiment3 = sentiment_analysis(text3)
 
-# Inferences
-sentiment1 = sentiment_analysis(text1)
-sentiment2 = sentiment_analysis(text2)
-sentiment3 = sentiment_analysis(text3)
-
-# assert the outptus
-assert sentiment1[0]['label'] == 'POSITIVE'
-assert sentiment2[0]['label'] == 'POSITIVE'
-assert sentiment3[0]['label'] == 'NEGATIVE'
+    assert sentiment1[0]['label'] == 'POSITIVE'
+    assert sentiment2[0]['label'] == 'POSITIVE'
+    assert sentiment3[0]['label'] == 'NEGATIVE'
